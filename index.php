f8f5348313aecdd607da648733d0bdca10771705
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-01-15 14:39:01 -0500

    fat free

--- index.php
+++ index.php
@@ -8,5 +8,16 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
+require("vendor/autoload.php");
+
+// :: means to invoke a static method. -> means to use an instance method
+$f3 = Base::instance();
+
+$f3->route('GET /', function() {
+    echo "Hello";
+});
+
+// Run F3
+$f3->run();
 
 
